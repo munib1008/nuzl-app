d4e3a08eb9bd88fd284df8f142019d7b70d7f8a9
munib1008 <mohammedmunib90@gmail.com>
2026-06-01 09:09:06 +0400

    Create mortgage_repository.dart

---
+++ lib/features/mortgage/data/mortgage_repository.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/network/api_client.dart';
+import '../domain/mortgage.dart';
+
+final mortgageRepositoryProvider =
+    Provider((ref) => MortgageRepository(ref.read(apiClientProvider)));
+
+final mortgagesProvider = FutureProvider.autoDispose<List<Mortgage>>((ref) async =>
+    ref.read(mortgageRepositoryProvider).list());
+
+final mortgageDetailProvider =
+    FutureProvider.autoDispose.family<Map<String, dynamic>, String>((ref, id) async =>
+        ref.read(mortgageRepositoryProvider).detail(id));
+
+final mortgagePaymentsProvider =
+    FutureProvider.autoDispose.family<List<MortgagePayment>, String>((ref, id) async =>
+        ref.read(mortgageRepositoryProvider).payments(id));
+
+class MortgageRepository {
+  MortgageRepository(this._api);
+  final ApiClient _api;
+
+  Future<List<Mortgage>> list() async {
+    final data = await _api.get('/mortgages');
+    return (data as List).map((e) => Mortgage.fromJson(Map<String, dynamic>.from(e))).toList();
+  }
+
+  Future<Map<String, dynamic>> detail(String id) async {
+    final data = await _api.get('/mortgages/$id');
+    return Map<String, dynamic>.from(data);
+  }
+
+  Future<List<MortgagePayment>> payments(String id) async {
+    final data = await _api.get('/mortgages/$id/payments');
+    return (data as List).map((e) => MortgagePayment.fromJson(Map<String, dynamic>.from(e))).toList();
+  }
+
+  Future<Mortgage> create(Map<String, dynamic> body) async {
+    final data = await _api.post('/mortgages', body: body);
+    return Mortgage.fromJson(Map<String, dynamic>.from(data));
+  }
+
+  Future<void> addPayment(String id, Map<String, dynamic> body) =>
+      _api.post('/mortgages/$id/payments', body: body);
+}
